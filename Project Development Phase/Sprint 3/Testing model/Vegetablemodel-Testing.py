66f76a4c526363950d44bd19b66acc4e7bc56916
DeviSriPrasadReddy <65356413+DeviSriPrasadReddy@users.noreply.github.com>
2022-11-12 11:19:09 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/Testing model/Vegetablemodel-Testing.py
@@ -0,0 +1,11 @@
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+model = load_model("veg.h5")
+img = image.load_img('bellpepper.JPG',target_size=(128,128))
+x=image.img_to_array(img)
+x=np.expand_dims(x,axis=0)
+pred=model.predict(x)
+classes=np.argmax(pred,axis=1)
+print(classes)
